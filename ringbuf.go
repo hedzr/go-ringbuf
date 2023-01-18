4ad5a141e2826c2eee659a3e15422db9a273bb72
hz <hedzrz@gmail.com>
2023-01-18 21:55:28 +0800

    added simple package-level doc

--- ringbuf.go
+++ ringbuf.go
@@ -1,8 +1,10 @@
+// Package ringbuf provides a multi-producer & multi-consumer (MPMC)
+// awared ring buffer in golang.
 package ringbuf
 
 import "github.com/hedzr/go-ringbuf/v2/mpmc"
 
 // New returns the RingBuffer object
 func New[T any](capacity uint32, opts ...mpmc.Opt[T]) mpmc.RingBuffer[T] {
-	return mpmc.New[T](capacity, opts...)
+	return mpmc.New(capacity, opts...)
 }
