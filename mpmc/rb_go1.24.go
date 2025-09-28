0b69880cac62e9902676b1435a6c7bbbb163de75
hz <hedzrz@gmail.com>
2025-09-28 10:08:17 +0800

    some skels for go 1.24

---
+++ mpmc/rb_go1.24.go
@@ -0,0 +1,7 @@
+package mpmc
+
+// func (rb *ringBuf[T]) Iterator(yield func(v T) bool) bool {
+// 	return
+// }
+
+func (rb *ringBuf[T]) Only124() string { return "OK, Go 1.24." }
---
+++ mpmc/rb_go1.24_test.go
@@ -0,0 +1,23 @@
+package mpmc
+
+import (
+	"testing"
+)
+
+// func (rb *ringBuf[T]) Iterator(yield func(v T) bool) bool {
+// 	return
+// }
+
+func TestRingBuf_Only124(t *testing.T) { //nolint:revive
+	size := roundUpToPower2(4)
+	t.Logf("start, size = %v", size)
+	q := &ringBuf[uint32]{
+		data:       make([]rbItem[uint32], size),
+		head:       0,
+		tail:       0,
+		cap:        size,
+		capModMask: size - 1,
+	}
+
+	t.Logf("[go1.23] Test %v", q.Only124())
+}
